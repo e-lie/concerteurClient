4d546d30dc8171dfa2cdccea7e146971b8d488dd
e-lie <eliegavoty@free.fr>
2017-04-19 18:46:52 +0000

    cron ready polling

--- polling.py
+++ polling.py
@@ -8,8 +8,8 @@ import os
 from glob import glob
 from pydub import AudioSegment
 
-SERVER_URL = os.environ['SERVER_URL']
-SOUND_DIR = ['SOUND_DIR']
+SERVER_URL="http://81.67.71.234:9000"
+SOUND_DIR="/home/pi/concerteurClient/sounds/"
 LAST = 'last_file_name.txt'
 
 def get_sound_list():
@@ -31,46 +31,34 @@ def get_sound_list():
 def get_sound(filename):
     url = SERVER_URL + '/get-sound'
     params = {'soundname':filename}
-    with open("sounds/"+filename, 'wb') as f:
+    with open(SOUND_DIR+filename, 'wb') as f:
         querystring = urllib.parse.urlencode(params)
         resp = request.urlopen(url, querystring.encode('utf-8'))
         mp3 = resp.read()
         f.write(mp3)
 
 
-def convert_sounds_to_wav():
-    mp3_files = glob(SOUND_DIR+"*.mp3")
-    for mp3 in mp3_files:
-        mp3seg = AudioSegment.from_mp3(mp3)
-        mp3seg.export(mp3.replace('.mp3','.wav'), format="mp3")
+def convert_sounds_to_wav(new_files):
+    mp3_files = glob(SOUND_DIR+'*.mp3')
+    file_number = len(mp3_files) - len(new_files)
+    for mp3_filename in new_files:
+        file_number += 1
+        mp3_path = SOUND_DIR+mp3_filename
+	
+        mp3seg = AudioSegment.from_mp3(mp3_path)
+        mp3seg.export("{}{}.wav".format(SOUND_DIR,file_number), format="wav")
+    
+    with open(SOUND_DIR+'params.txt', 'w') as f:
+        f.write(str(len(mp3_files)))
 
-def write_sound_list():
-    with open(SOUND_DIR+"sound_list.txt", 'wt') as f:
-        wav_files = glob(SOUND_DIR+"*.wav")
-        #to sort files by their id keep only the filename
-        sortkey = lambda filename: int(parse.parse("{}_{}_{}",
-                                        filename.replace(SOUND_DIR,''))[0])
-        wav_files.sort(key = sortkey)
-        i = 0
-        mode = "w"
-        for wav in wav_files:
-            if i>0:
-                mode = "a"
-            with open(SOUND_DIR+"sound_list.txt",mode) as f:
-                f.write("{} {};\n".format(i,wav))
-            i+=1
-
-        with open(SOUND_DIR+"params.txt",'w') as f:
-            f.write("{} {};\n".format('file_count',i))
 
 if __name__ == "__main__":
     data = get_sound_list()
-    soundList = data['filenames']
-    lastFilename = data['lastfilename']
-    for sound in soundList:
+    new_files = data['filenames']
+    last_filename = data['lastfilename']
+    for sound in new_files:
         get_sound(sound)
-    with open('last_file_name.txt','w') as f:
-        f.write(lastFilename)
+    with open(SOUND_DIR+LAST, 'w') as f:
+        f.write(last_filename)
 
-    convert_sounds_to_wav(SOUND_DIR)
-    write_sound_list(SOUND_DIR)
+    convert_sounds_to_wav(new_files)
